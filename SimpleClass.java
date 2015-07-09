f573daf9b72100791f58d3b6ff7357b760fb8fb0
Hajra Murtaza <hajra.murtaza@riphah.edu.pk>
2015-07-09 11:19:23 +0500

    Revert "Changes to Simple Class"
    This reverts commit a3e7fd2aa61f56f69e8f6776b467d7c3abf2079c.
    
    Conflicts:
    	SimpleClass.java

--- SimpleClass.java
+++ SimpleClass.java
@@ -1,8 +1,11 @@
 public class SimpleClass{
 	public static void main(String args[]){
 		System.out.println("Git WOrkshop");
+<<<<<<< HEAD
 System.out.println("-------------------");
 //just a comment
 System.in.read();
+=======
+>>>>>>> parent of a3e7fd2... Changes to Simple Class
 	}
 }
